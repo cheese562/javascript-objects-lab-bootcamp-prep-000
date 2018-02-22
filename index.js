0ba817845c9300817b978bf569e702aab9d39d9f
Leon K <leonknight@outlook.com>
2018-02-22 04:42:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+  // returns an object with the orignal key value pairs and the new key value pair
+  reci
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  //'updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object'
+}
+
+function deleteFromObjectByKey(object, key)
+{
+  // 'deletes `key` from a clone of object and returns the new object (it is non-destructive)'
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  // returns object without the delete key/value pair
+}
